4c53bc8d2b92ff02ef8c9df18500ef71b8097f2e
Victor Hernandez <vikhhr@gmail.com>
2017-07-19 00:22:01 -0600

    Adding thumbnails

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -64,6 +64,20 @@ Instructions:
 
     Your code goes here!
      */
-    // getJSON('../data/earth-like-results.json')
+    getJSON('../data/earth-like-results.json')
+      .then(function(response) {
+        addSearchHeader(response.query);
+        return getJSON(response.results[0]);
+      })
+      .catch(function() {
+        throw Error('Search Request Error');
+      })
+      .then(function(planetData) {
+        createPlanetThumb(planetData);
+      })
+      .catch(function(e) {
+        addSearchHeader('unknown');
+        console.log(e);
+      })
   });
 })(document);
