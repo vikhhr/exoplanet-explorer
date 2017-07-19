56a4e3d83a58c92e9c1d515f26fba5b9a13743f0
Victor Hernandez <vikhhr@gmail.com>
2017-07-18 21:39:55 -0600

    Implementing fetch usage.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -35,6 +35,9 @@ Instructions:
 
     Your code goes here!
      */
+    return fetch(url, {
+      method: 'get'
+    });
   }
 
   /**
@@ -48,6 +51,11 @@ Instructions:
 
     Your code goes here!
      */
+
+     return get(url)
+      .then(function(response) {
+        return response.json();
+      });
   }
 
   window.addEventListener('WebComponentsReady', function() {
@@ -58,6 +66,14 @@ Instructions:
 
     Your code goes here too!
      */
-    // getJSON('../data/earth-like-results.json')
+    getJSON('../data/earth-like-results.json')
+      .then(function(response) {
+        addSearchHeader(response.query);
+        console.log(response);
+      })
+      .catch(function(error) {
+        addSearchHeader('unknown');
+        console.log(error);
+      })
   });
 })(document);
